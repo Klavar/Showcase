3b68d86090a4d15c65df9812261b5ed0194f7fa2
Tony Merritt <Klavar@Meninx.home>
2016-10-05 13:22:19 +0100

    Initial Commit

---
+++ Klavar-Showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Klavar-Showcase
+//
+//  Created by Tony Merritt on 05/10/2016.
+//  Copyright © 2016 Tony Merritt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
